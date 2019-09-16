78359752cd50ab733e228b1a098e949eba634343
Karan Khatnani <karankhatnan@hotmail.com>
2019-09-16 11:45:19 +1000

    Updated web.xml

--- src/source/script/ViewProfile.java
+++ src/source/script/ViewProfile.java
@@ -10,7 +10,7 @@ import source.domain.*;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ViewProfile
  */
-@WebServlet("/ViewProfile")
+
 public class ViewProfile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
